b1e746cc04377f3e240c136ccf55b3dbea3cdc6f
Mark Kornblum <mark.kornblum@gmail.com>
2012-12-16 12:29:31 -0800

    map/reduce across whole client collection to display total px

--- px.js
+++ px.js
@@ -7,24 +7,32 @@ if (Meteor.isClient) {
     return viewportWidth * viewportHeight;
   }
 
-  function updatePx(){
+  function updateClientPx(){
     clientPx = calculatePx();
     Clients.update({_id: clientId}, {px: clientPx});
   }
 
   var clientPx = calculatePx();
-
   var clientId = Clients.insert({px: clientPx});
 
-  var count = Meteor.render(function(){
+  Meteor.render(function(){
     Template.hello.count = function () {
       return Clients.findOne({_id: clientId}).px
+    };
+
+    Template.hello.allCount = function(){
+      clients = Clients.find().map(function(client){
+        return client.px;
+      });
+
+      return clients.reduce(function(a, b){
+        return a + b;
+      });
     };
   });
 
   window.onresize = function() {
-      console.log('resize triggered');
-      updatePx();
+    updateClientPx();
   }
 }
 
